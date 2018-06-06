0f75a76678ebd1ad4561a74658a5ecc33dfc100d
Jozef Knaperek <jknaperek@gmail.com>
2018-06-06 11:45:48 +0200

    Configure genesis.json with mainnet keys and accounts

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t onegram-testnet --build-arg BUILD=Release --build-arg REVISION_SHA=`git rev-parse --short HEAD` --build-arg REVISION_TIMESTAMP=`git log -1 --format=%at` -f Dockerfile.OGC .
+docker build -t onegram/onegram-core --build-arg BUILD=Release --build-arg REVISION_SHA=`git rev-parse --short HEAD` --build-arg REVISION_TIMESTAMP=`git log -1 --format=%at` -f Dockerfile.OGC .
