d2912f37ce703439ef6d5acddca7aee864c289d6
Mario <2648314-mariocrypto@users.noreply.gitlab.com>
2019-01-25 15:18:12 +0100

    Add seed nodes whitelisting option

--- libraries/net/include/graphene/net/peer_database.hpp
+++ libraries/net/include/graphene/net/peer_database.hpp
@@ -108,6 +108,8 @@ namespace graphene { namespace net {
     potential_peer_record lookup_or_create_entry_for_endpoint(const fc::ip::endpoint& endpointToLookup);
     fc::optional<potential_peer_record> lookup_entry_for_endpoint(const fc::ip::endpoint& endpointToLookup);
 
+    void peer_database_as_whitelisted(bool whitelisted);
+
     typedef detail::peer_database_iterator iterator;
     iterator begin() const;
     iterator end() const;
