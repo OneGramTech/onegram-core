de1df362777dc473276e985079cb064a23dae107
CryptoCoder <cryptocoder@nomail.com>
2019-03-08 11:34:23 +0100

    Updated build scripts to support OpenSSL 1.1+ on Windows/MSVC toolchain

--- tools/build-win/build_openssl.bat
+++ tools/build-win/build_openssl.bat
@@ -26,13 +26,33 @@ REM Check end
 echo Setting up VS2015 environment...
 call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
 
-echo Building OpenSSL...
+:preinstall
+IF "%__BUILD_VERSION%" GEQ "openssl-1.1.0" (
+    goto openssl_install_11
+)
+
+:openssl_install_10
+echo Building OpenSSL ( version 1.0 build ) ...
 cd %OPENSSL_ROOT%
 perl Configure VC-WIN64A --prefix=%OPENSSL_ROOT% --openssldir=%OPENSSL_ROOT%
 call ms\do_win64a.bat
 nmake -f ms\ntdll.mak
 set BUILD_RESULT=%ERRORLEVEL%
 nmake -f ms\ntdll.mak install
+goto postinstall
+
+:openssl_install_11
+echo Building OpenSSL ( version 1.1 build ) ...
+cd %OPENSSL_ROOT%
+perl Configure VC-WIN64A no-shared --prefix=%OPENSSL_ROOT% --openssldir=%OPENSSL_ROOT%
+nmake
+set BUILD_RESULT=%ERRORLEVEL%
+REM nmake test
+REM nmake install
+echo OpenSSL 1.1 build result: %BUILD_RESULT%
+goto postinstall
+
+:postinstall
 
 popd
 
